f4a10523194df306be7818e73715f8eed6cc216e
Muskan Singh <muskansngh15@gmail.com>
2025-06-27 16:30:45 +0530

    Make a string good if two adjacent characters are same but in diff cases.

---
+++ Stack/goodString.java
@@ -0,0 +1,20 @@
+```java
+  class Solution {
+    public String makeGood(String s) {
+        Stack<Character> sck=new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(!sck.isEmpty() && Math.abs(ch-sck.peek())==32)
+                sck.pop();
+            else
+                sck.push(s.charAt(i));
+        }
+        StringBuilder sb=new StringBuilder();
+        for(char c:sck)
+        {
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
