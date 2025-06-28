f68e7318238abf9d6132cdce4785676bf4e9de4e
Muskan Singh <muskansngh15@gmail.com>
2025-06-28 23:26:27 +0530

    Items matching a rule in a nested array list.

---
+++ String/itemsMatchingRule.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int count=0;
+        for(int i=0;i<items.size();i++)
+        {
+            if(ruleKey.equals("type") && ruleValue.equals(items.get(i).get(0)))
+                count++;
+            if(ruleKey.equals("color") && ruleValue.equals(items.get(i).get(1)))
+                count++;
+            if(ruleKey.equals("name") && ruleValue.equals(items.get(i).get(2)))
+                count++;
+        }
+        return count;
+    }
+}
